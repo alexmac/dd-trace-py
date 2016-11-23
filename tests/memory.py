04ef2c4a62f994f8241dc14e2d81f7cf88643870
Matt Perpick <matthewperpick@gmail.com>
2016-11-23 01:30:37 +0000

    more kitchen sink

--- tests/memory.py
+++ tests/memory.py
@@ -12,6 +12,8 @@ import sys
 
 # 3p
 import pympler.tracker
+import pymongo
+import pylibmc
 import psycopg2
 import redis
 
@@ -20,7 +22,7 @@ import ddtrace
 from tests.contrib import config
 
 
-ddtrace.patch(redis=True)
+ddtrace.patch_all()
 ddtrace.tracer.writer = None
 
 
@@ -29,10 +31,17 @@ class KitchenSink(object):
     def __init__(self):
         self._redis = redis.Redis(**config.REDIS_CONFIG)
         self._pg = psycopg2.connect(**config.POSTGRES_CONFIG)
+        self._pymongo = pymongo.MongoClient(**config.MONGO_CONFIG)
+        self._pylibmc = pylibmc.Client(["%s:%s" % (
+            config.MEMCACHED_CONFIG["host"],
+            config.MEMCACHED_CONFIG["port"])])
+
 
     def ping(self, i):
-        self._ping_redis(i)
-        self._ping_pg(i)
+        with ddtrace.tracer.trace("kitchensink", service="kitchensink"):
+            self._ping_redis(i)
+            self._ping_pg(i)
+            self._ping_pymongo(i)
 
     def _ping_redis(self, i):
         with self._redis.pipeline() as p:
@@ -47,6 +56,15 @@ class KitchenSink(object):
             cur.fetchall()
         finally:
             cur.close()
+
+    def _ping_pymongo(self, i):
+        db = self._pymongo["test-db"]
+        db.teams.find({"name": "Toronto Maple Leafs"})
+
+    def _ping_pylibmc(self, i):
+        self._pylibmc.set("a", 1)
+        self._pylibmc.incr("a", 2)
+        self._pylibmc.decr("a", 1)
 
 
 if __name__ == '__main__':
